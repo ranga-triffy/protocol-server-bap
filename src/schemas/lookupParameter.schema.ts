9f0fa603d0ccb378df7f8407bb75cde28f559931
AyushAgrawal25 <agrawal.ayush2500@gmail.com>
2022-03-28 21:12:40 +0530

    Lookup cache Implemented

--- src/schemas/lookupParameter.schema.ts
+++ src/schemas/lookupParameter.schema.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { NetworkPaticipantType } from "./subscriberDetails.schema";
 
 export const lookupParameterSchema = z.object({
-    subscriber_id: z.string().optional().nullable(),
-    unique_key_id: z.string().optional().nullable(),
-    type: z.nativeEnum(NetworkPaticipantType).optional().nullable(),
-    domain: z.string().optional().nullable(),
-})
+  subscriber_id: z.string().optional().nullable(),
+  unique_key_id: z.string().optional().nullable(),
+  type: z.nativeEnum(NetworkPaticipantType).optional().nullable(),
+  domain: z.string().optional().nullable()
+});
 
 export type LookupParameter = z.TypeOf<typeof lookupParameterSchema>;
